4e93d625521f2f30c88f00952692c10461f1f846
Vedashree20 <79038911+Vedashree20@users.noreply.github.com>
2021-10-31 15:58:02 +0530

    Delete email_trigger.sql

--- project/email_trigger.sql
+++
@@ -1,9 +0,0 @@
-/*CREATE TABLE testrigger
-(
-   status varchar(255)
-);------>create a table to check insertion of records after creating main table*/
-create trigger sample_trigger
-after insert
-on company
-for each row
-insert into testrigger values('inserted record');
